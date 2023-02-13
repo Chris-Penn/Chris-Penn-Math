fa989058fcbc7161699e79f323f13d431a015457
Chris Penn <madongola@gmail.com>
2023-02-13 19:48:03 +0000

    Completed Lab Math

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -20,6 +20,8 @@ public class Lab {
      * @return return a^b using the pow() method of the Math class.
      */
     public double mathPow(double a, double b){
-        return 0;
+
+        double exp = Math.pow(a,b);
+        return exp;
     }
 }
